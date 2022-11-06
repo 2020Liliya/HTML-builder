2f84994decd69d115ade03957cb5929dabb866eb
Lily Trofimova <trofimovaliliy4@gmail.com>
2022-11-06 17:42:15 +0700

    feat: add 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+const filesDirPath = path.resolve(__dirname, './files');
+const filesCopyDirPath = path.resolve(__dirname, './files-copy');
 
+fsPromises.mkdir(filesCopyDirPath, { recursive: true });
+
+fs.readdir(filesDirPath, (err, files) => {
+    if (err)
+        console.log(err);
+    else {
+        files.forEach(file => {
+            fsPromises.copyFile(path.resolve(filesDirPath, file), path.resolve(filesCopyDirPath, file));
+        });
+    }
+});
