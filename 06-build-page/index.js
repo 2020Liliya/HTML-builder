1e616e64e241c97432c9b66fd722caf27cce8ae7
Lily Trofimova <trofimovaliliy4@gmail.com>
2022-11-08 01:24:31 +0700

    feat: add 06 task

---
+++ 06-build-page/index.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
+const stylesPath = path.resolve(__dirname, './styles');
+const distPath = path.join(__dirname, './project-dist');
+fsPromises.mkdir(distPath, { recursive: true });
+const cssDistPath = path.join(distPath, './style.css');
+
+const assetsPath = path.resolve(__dirname, './assets');
+const assetsDistPath = path.resolve(distPath, './assets');
+
+const componentsPath = path.resolve(__dirname, './components');
+const index = path.join(distPath, './index.html');
+
+
+// создать файл css в dist
+let arrStyles = [];
+fs.readdir(stylesPath,
+    { withFileTypes: true },
+    (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                if (file.isFile() && path.extname(file.name) === '.css') {
+                    arrStyles.push(file.name);
+                }
+            });
+        }
+
+        fsPromises.writeFile(cssDistPath, "");
+
+        for (let style of arrStyles) {
+            const input = fs.createReadStream(path.resolve(stylesPath, style), "utf-8");
+            fsPromises.appendFile(cssDistPath, input);
+        }
+    })
+
+
+// скопировать папку assets в dist
+async function copyDir(input, output) {
+    await fsPromises.mkdir(output, { recursive: true });
+    let entries = await fsPromises.readdir(input, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let inputPath = path.join(input, entry.name);
+        let outputPath = path.join(output, entry.name);
+
+        if (entry.isDirectory()) {
+            await copyDir(inputPath, outputPath);
+        } else {
+            await fsPromises.copyFile(inputPath, outputPath);
+        }
+    }
+}
+copyDir(assetsPath, assetsDistPath);
+
+
+// массив компонентов
+let arrComponents = [];
+fs.readdir(componentsPath,
+    { withFileTypes: true },
+    (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                if (file.isFile() && path.extname(file.name) === '.html') {
+                    arrComponents.push(file.name);
+                }
+            });
+        }
+    });
+
+// заменить шаблонные теги
+fs.readFile(path.resolve(__dirname, './template.html'), "utf-8", (error, fileContent) => {
+    if (error) {
+        console.log('No such file exists');
+    }
+
+    let indexContent = fileContent;
+
+    for (let style of arrComponents) {
+        fs.readFile(path.resolve(componentsPath, style), 'utf8', (errComponent, fileContentComponent) => {
+            if (errComponent) throw errComponent;
+            indexContent = indexContent.replace(`{{${style.replace('.html', '')}}}`, fileContentComponent);
+
+            fs.writeFile(index, indexContent, function (error) {
+                if (error) throw error;
+                console.log(`{{${style.replace('.html', '')}}}`, 'Success!');
+            });
+        });
+    }
+});
